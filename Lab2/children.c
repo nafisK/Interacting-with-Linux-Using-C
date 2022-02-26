bd50eef9ae79348087d8ac856b15a234a0f65042
Nafis Khan <49691160+nafisK@users.noreply.github.com>
2022-02-26 15:53:57 -0500

    Implemented lab 2.1

---
+++ Lab2/children.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+/*
+Assignment 2 by Nafis Khan
+*/
+
+int main (int argc, char* argv[]) {
+
+    // checks if correct arguments are given
+    if(1 != argc) {
+        printf("\nPlease provide appropriate arguments.\n");
+        return 1;
+    }
+
+    // printf("Parent Process, PID: %d\n", getpid());
+    pid_t statusOne, statusTwo;
+
+    // forking to create a new child process
+    int childOne = fork();
+
+    // If childOne != 0, then parent process
+    // else child process
+	if (childOne != 0){
+
+        // creates child two
+        int childTwo = fork();
+
+        // waits for childTwo to be terminated
+        waitpid(childTwo, &statusTwo, 0);
+
+        // if childTwo == 0, then it is the child, else parent
+        if (childTwo == 0) {
+            printf("I am child two, my PID is: %d\n", getpid());
+        }
+        else {
+            // wait for child one to be terminated
+            waitpid(childOne, &statusOne, 0);
+            // this will get executed after both children have been terminated
+            printf("I am the parent with PID %d\n", getpid());
+        }
+        
+    }else {
+        // print that you are the child
+        printf("I am child one, my PID is: %d\n", getpid());
+    }
+    
+    
+    return 0;
+}
