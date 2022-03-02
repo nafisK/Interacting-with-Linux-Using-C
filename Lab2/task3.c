b482b45e0964d5855a5cae7f533ab58d19d495bb
Nafis Khan <49691160+nafisK@users.noreply.github.com>
2022-03-02 16:59:36 -0500

    guide for number 3

---
+++ Lab2/task3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+/*
+Assignment 2 by Nafis Khan
+*/
+
+int main (int argc, char* argv[]) {
+
+    // create child one
+        // use argument and mod to calculate sum
+        // print sum
+    // create child two
+        // use argument and mod to calculate sum
+        // print sum
+    // wait for child to finish
+        // use wait
+    // use parent
+        // use argument and mod to calculate sum
+        // print sum
+
+
+    return 0;
+}
