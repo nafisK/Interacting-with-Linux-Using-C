fd0fefaaec84e16aab0cc132e30b99906848d9a3
Nafis Khan <49691160+nafisK@users.noreply.github.com>
2022-03-03 13:09:46 -0500

    started task 4

---
+++ Lab2/tasl4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+/*
+Assignment 2 by Nafis Khan
+*/
+
+int main (int argc, char* argv[]) {
+
+    // checks if correct arguments are given
+    if(2 != argc) {
+        printf("\nPlease provide appropriate arguments.\n");
+        return 1;
+    }
+
+    
+
+    return 0;
+}
