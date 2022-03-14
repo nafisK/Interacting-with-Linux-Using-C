8fc822add439f043c7e9e712a9b1ef328ffd2ce9
Nafis Khan <49691160+nafisK@users.noreply.github.com>
2022-03-13 22:00:29 -0400

    completed lab3 task2

--- Lab3/task2.c
+++ Lab3/task2.c
@@ -9,6 +9,27 @@
 
 int main(void){	
     
+	int status, pID;
 
+    // creating new child process
+	pID = fork();
+
+    // waits for child process to go first
+	wait(&status);
+
+    // parent
+	if(pID == 0){
+		printf("Child Forked Successfully, PID: %d\n", getpid());
+
+        // prints all the necessary info
+        // -l for cleaner view
+        // -a for hidden files
+        // -x for files only in currect folder
+        // -o for removing unnecessary info
+		char *argv[] = {"ls", "-laxo", NULL};
+		execvp(argv[0], argv);
+		printf("execvp Failed\n");	
+	}
+	return 0;
 
 }
